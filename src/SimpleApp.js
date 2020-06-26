db04f3b95ccb6b88691bd08b421849cb43b817d8
Ikuo Terado <eqobar@gmail.com>
2020-06-27 03:11:47 +0900

    Minor change

--- src/SimpleApp.js
+++ src/SimpleApp.js
@@ -1,7 +1,5 @@
 import React from "react"
 
-import "./styles.css"
-
 class BleDevice {
   device = null
   characteristics = {}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom"
 
 // import App from "./App"
 import App from "./SimpleApp"
+import "./styles.css"
 
 const rootElement = document.getElementById("root")
 ReactDOM.render(
